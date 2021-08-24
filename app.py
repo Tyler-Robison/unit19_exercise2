a9c0ebc09c5163cc91dba57aa132b639b21e0989
Tyler Robison <tylerobison758758@gmail.com>
2021-08-24 15:46:29 -0400

    fixed a bug

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import Flask, request, render_template
 from flask_debugtoolbar import DebugToolbarExtension
 from stories import Story
 
-#We are currently importing story instance not Story Class.
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "chickensarecool123456"
 
@@ -15,33 +13,17 @@ story = Story(
        large {adjective} {noun}. It loved to {verb} {plural_noun}."""
 )
 
-# story.prompt
-# story.generate()
-# As explained in e-mail, I can't access story methods/attrs in global scope, only function scope
-# That is why I have the class instantiated inside my functions instead of global scope.
 
 @app.route('/')
 def home_page():
 
-
-    story = Story(
-    ["place", "noun", "verb", "adjective", "plural_noun"],
-    """Once upon a time in a long-ago {place}, there lived a
-       large {adjective} {noun}. It loved to {verb} {plural_noun}."""
-)
-
     words = story.prompts
-    story
+
     return render_template('madlibs.html', my_words=words)
 
 @app.route('/story')
-def story():
+def generate_story():
 
-    story = Story(
-    ["place", "noun", "verb", "adjective", "plural_noun"],
-    """Once upon a time in a long-ago {place}, there lived a
-       large {adjective} {noun}. It loved to {verb} {plural_noun}."""
-)
     test_dict = {}
 
     for (key, value) in request.args.items():
